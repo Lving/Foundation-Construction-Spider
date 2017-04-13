bdc4c14715bbdcd8c3ba1af90dba5b9bc66bb42f
Lving <qianchao0928@foxmail.com>
2017-04-13 17:37:51 +0800

    deploy on scrapinghub

--- jijian_spider/pipelines.py
+++ jijian_spider/pipelines.py
@@ -4,7 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import pymongo
+# import pymongo
 from scrapy.exceptions import DropItem
 
 
@@ -13,34 +13,34 @@ class JijianSpiderPipeline(object):
         return item
 
 
-class MongoPipeline(object):
-    """
-    入库
-    """
-    collection_name = 'jijian_tong'
-
-    def __init__(self, mongo_uri, mongo_db):
-        self.mongo_uri = mongo_uri
-        self.mongo_db = mongo_db
-
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        return cls(
-            mongo_uri=crawler.settings.get("MONGO_URI"),
-            mongo_db=crawler.settings.get("MONGO_DATABASE", 'items')
-        )
-
-    def open_spider(self, spider):
-        self.client = pymongo.MongoClient(self.mongo_uri)
-        self.db = self.client[self.mongo_db]
-
-    def close_spider(self, spider):
-        self.client.close()
-
-    def process_item(self, item, spider):
-        self.db[self.collection_name].update({'id': item['id']}, dict(item), True)
-        return item
+# class MongoPipeline(object):
+#     """
+#     入库
+#     """
+#     collection_name = 'jijian_tong'
+#
+#     def __init__(self, mongo_uri, mongo_db):
+#         self.mongo_uri = mongo_uri
+#         self.mongo_db = mongo_db
+#
+#
+#     @classmethod
+#     def from_crawler(cls, crawler):
+#         return cls(
+#             mongo_uri=crawler.settings.get("MONGO_URI"),
+#             mongo_db=crawler.settings.get("MONGO_DATABASE", 'items')
+#         )
+#
+#     def open_spider(self, spider):
+#         self.client = pymongo.MongoClient(self.mongo_uri)
+#         self.db = self.client[self.mongo_db]
+#
+#     def close_spider(self, spider):
+#         self.client.close()
+#
+#     def process_item(self, item, spider):
+#         self.db[self.collection_name].update({'id': item['id']}, dict(item), True)
+#         return item
 
 
 class DuplicatesPipeline(object):
